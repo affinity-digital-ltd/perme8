7f91bfc79f7c000a1c78d9a6d5eb8c091eff2e1c
Kris Quigley <5502215+krisquigley@users.noreply.github.com>
2025-11-06 17:47:44 +0000

    chore: exclude eval tests by default

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,5 @@
-ExUnit.start()
+# Exclude evaluation tests by default
+# To run evaluation tests: mix test --include evaluation
+ExUnit.start(exclude: [:evaluation])
+
 Ecto.Adapters.SQL.Sandbox.mode(Jarga.Repo, :manual)
